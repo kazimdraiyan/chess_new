f50ed302b5733674de826a1fae92d4732e8a62d0
Raiyan <kazimdraiyan2006@gmail.com>
2025-02-19 23:39:30 +0600

    pawn legal move implementation excluding en passant

--- lib/models/board_analyzer.dart
+++ lib/models/board_analyzer.dart
@@ -19,6 +19,8 @@ class BoardAnalyzer {
       return _queenLegalMoves(square);
     } else if (piece.pieceType == PieceType.knight) {
       return _knightLegalMoves(square);
+    } else if (piece.pieceType == PieceType.pawn) {
+      return _pawnLegalMoves(square);
     } else {
       // TODO: Implement other piece moves
       return [];
@@ -85,6 +87,49 @@ class BoardAnalyzer {
     return result;
   }
 
+  List<Square> _pawnLegalMoves(Square square) {
+    final pawnSquares = <Square>[];
+    final pawnCapturableSquares = <Square>[];
+
+    final piece = _piecePlacement.pieceAt(square)!;
+
+    final rankStep = piece.isWhite ? 1 : -1;
+
+    // 1-step to the front
+    pawnSquares.add(Square(square.file, square.rank + rankStep));
+
+    // 2-step to the front if the pawn is at its starting position.
+    if ((piece.isWhite && square.rank == 2) ||
+        (!piece.isWhite && square.rank == 7)) {
+      pawnSquares.add(Square(square.file, square.rank + rankStep * 2));
+    }
+
+    // TODO: Extract the following part to another method which returns pawnAttackSquares
+    // 1-step diagonal to the front
+    final testingRank = square.rank + rankStep;
+    for (var fileStep = -1; fileStep <= 1; fileStep++) {
+      if (fileStep == 0) continue;
+
+      final testingFile = square.file + fileStep;
+      if (testingFile < 1 || testingFile > 8) continue;
+
+      final testingSquare = Square(testingFile, testingRank);
+      print(testingSquare);
+      if (isOccupiedByEnemyPiece(testingSquare, piece.isWhite)) {
+        pawnCapturableSquares.add(testingSquare);
+      }
+    }
+
+    return [
+      ...singleDirectionTraverseTillBlockage(
+        pawnSquares,
+        piece.isWhite,
+        isPawn: true,
+      ),
+      ...pawnCapturableSquares,
+    ];
+  }
+
   List<Square> traverseTillBlockage(
     List<List<Square>> directionalSquares,
     bool isOriginalPieceWhite,
@@ -92,19 +137,34 @@ class BoardAnalyzer {
     final result = <Square>[];
 
     for (final singleDirectionSquares in directionalSquares) {
-      for (final testingSquare in singleDirectionSquares) {
-        if (_piecePlacement.isEmpty(testingSquare)) {
-          result.add(testingSquare);
+      singleDirectionTraverseTillBlockage(
+        singleDirectionSquares,
+        isOriginalPieceWhite,
+      );
+    }
+    return result;
+  }
+
+  List<Square> singleDirectionTraverseTillBlockage(
+    List<Square> singleDirectionSquares,
+    bool isOriginalPieceWhite, {
+    bool isPawn = false,
+  }) {
+    final result = <Square>[];
+
+    for (final testingSquare in singleDirectionSquares) {
+      if (_piecePlacement.isEmpty(testingSquare)) {
+        result.add(testingSquare);
+      } else {
+        if (isOccupiedByFriendlyPiece(testingSquare, isOriginalPieceWhite)) {
+          break; // Blocked by a friendly piece, so continue to traverse on the next direction
         } else {
-          if (isOccupiedByFriendlyPiece(testingSquare, isOriginalPieceWhite)) {
-            break; // Blocked by a friendly piece, so continue to traverse on the next direction
-          } else {
-            result.add(testingSquare);
-            break; // Blocked by a enemy piece, so include the square and continue to traverse on the next direction
-          }
+          if (!isPawn) result.add(testingSquare);
+          break; // Blocked by a enemy piece, so include the square and continue to traverse on the next direction
         }
       }
     }
+
     return result;
   }
 
@@ -116,5 +176,10 @@ class BoardAnalyzer {
             isOriginalPieceWhite) ||
         (_piecePlacement.isOccupiedByBlack(testingSquare) &&
             !isOriginalPieceWhite);
+  }
+
+  bool isOccupiedByEnemyPiece(Square testingSquare, bool isOriginalPieceWhite) {
+    return !_piecePlacement.isEmpty(testingSquare) &&
+        !isOccupiedByFriendlyPiece(testingSquare, isOriginalPieceWhite);
   }
 }
