3af87aae1705a1bc87b71578af71349a9a4d0ece
Raiyan <kazimdraiyan2006@gmail.com>
2025-02-19 14:45:49 +0600

    integrate starting position on the board

--- lib/models/board_manager.dart
+++ lib/models/board_manager.dart
@@ -1,1 +1,5 @@
-class BoardManager {}
+import 'package:chess_new/models/piece_placement.dart';
+
+class BoardManager {
+  PiecePlacement currentPiecePlacement = PiecePlacement.starting();
+}
--- lib/widgets/board.dart
+++ lib/widgets/board.dart
@@ -1,5 +1,5 @@
 import 'package:chess_new/constants.dart';
-import 'package:chess_new/models/piece.dart';
+import 'package:chess_new/models/board_manager.dart';
 import 'package:chess_new/models/square.dart';
 import 'package:chess_new/utils.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +13,8 @@ class Board extends StatefulWidget {
 }
 
 class _BoardState extends State<Board> {
+  final boardManager = BoardManager();
+
   var isWhiteToMove = true;
   var isWhitePerspective = true;
 
@@ -29,6 +31,11 @@ class _BoardState extends State<Board> {
       itemBuilder: (context, i) {
         final id = isWhitePerspective ? i : 63 - i;
         final square = Square.fromId(id);
+        final piece =
+            boardManager.currentPiecePlacement.pieceMatrix.reversed
+                .toList()[square.rank - 1][square.file -
+                1]; // Needed to reverse the pieceMatrix because GridView is reversed, i.e., it renders the grid from bottom to top, left to right.
+
         return Container(
           color:
               square.isDark
@@ -37,12 +44,13 @@ class _BoardState extends State<Board> {
           child: Center(
             child: Stack(
               children: [
-                Opacity(
-                  opacity: 0.5,
-                  child: SvgPicture.asset(
-                    Utils.iconSrcOf(PieceType.bishop),
+                if (piece != null)
+                  Opacity(
+                    opacity: 0.5,
+                    child: SvgPicture.asset(
+                      Utils.iconSrcOf(piece.pieceType, isWhite: piece.isWhite),
+                    ),
                   ),
-                ),
                 Center(child: Text(square.algebraicNotation)),
               ],
             ),
